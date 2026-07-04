26996dc5b34261f78a38ca8d6b515a9e47def439
PXC-PUC <pedro.xc@puccampinas.edu.br>
2026-07-04 19:33:05 -0300

    fix: corrigir alinhamento da seta de voltar e espaçamento da Toolbar

--- app/src/main/java/com/pedro/cinelog/MainActivity.kt
+++ app/src/main/java/com/pedro/cinelog/MainActivity.kt
@@ -2,22 +2,57 @@ package com.pedro.cinelog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.pedro.cinelog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        aplicarInsetsDeSistema()
+
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.searchFragment, R.id.diaryFragment, R.id.statsFragment)
+        )
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    /** Evita que a Toolbar e o BottomNavigationView fiquem embaixo da barra de status/navegação do sistema. */
+    private fun aplicarInsetsDeSistema() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.toolbar) { view, windowInsets ->
+            val barrasDeSistema = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = barrasDeSistema.top)
+            windowInsets
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(binding.bottomNavigationView) { view, windowInsets ->
+            val barrasDeSistema = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(bottom = barrasDeSistema.bottom)
+            windowInsets
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        return navHostFragment.navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
